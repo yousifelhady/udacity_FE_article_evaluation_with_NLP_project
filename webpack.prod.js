814ebad55b9c26981eccb942750917a476c2fd7b
yousifelhady <yousif.abdelhamed@gmail.com>
2021-03-23 18:30:23 +0200

    updating package.json with author and description, updating webpack.prod.js with required loaders and plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,22 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     module: {
         rules: [
-            // TODO 1: Add babel Loader that match js files as development
-            // TODO 2: Add Loaders for
-            //    1. converting sass => css
-            //    2. Turns css into commonjs
-            //    3. Extract css into files
-            /* HINT: structure
-        {
-          test: REGEX_TO_MATCH_FILES ex. /\.js$/,
-          exclude: /node_modules/,
-          loader: '',
-        }
-       */
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
         ]
     },
     plugins: [
@@ -29,6 +28,7 @@ module.exports = {
         // TODO: configure workbox-webpack-plugin
     ],
     optimization: {
-        // TODO: Add Optimization for JS and CSS
+        minimizer: [new TerserPlugin({}), 
+            new OptimizeCSSAssetsPlugin({})]
     }
 }
