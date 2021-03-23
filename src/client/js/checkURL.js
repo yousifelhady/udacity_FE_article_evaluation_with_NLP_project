dea9dc54e2cf660d80d5ef4170bb4a757ec5186b
yousifelhady <yousif.abdelhamed@gmail.com>
2021-03-23 22:57:24 +0200

    removing unnecessary comments

--- src/client/js/checkURL.js
+++ src/client/js/checkURL.js
@@ -1,13 +1,3 @@
-// TODO declare a function to check the url ..
-//  The question now how can i do it. I understand you are very busy mate so here's some hints pick the one you like
-/* HINTS
-    1. https://gist.github.com/franciskim/41a959f8e3989254ef5d
-    2. https://www.tutorialspoint.com/How-to-validate-URL-address-in-JavaScript
-    3. https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
-    4. https://www.codegrepper.com/code-examples/javascript/javascript+validate+url
-    5. https://stackoverflow.com/a/5717133/6483379
-    6. https://www.npmjs.com/package/valid-url
- */
 function isValidUrl(url) {
     var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
     var regExp = new RegExp(expression);
