feed5b7880faef5199c6c983fc1b730e5947bb35
yousifelhady <yousif.abdelhamed@gmail.com>
2021-03-24 01:25:43 +0200

    removing commented code in testFormHandler.spec.js

--- src/client/__test__/testFormHandler.spec.js
+++ src/client/__test__/testFormHandler.spec.js
@@ -4,14 +4,4 @@ describe('testing the submit functionality', () => {
     test("testing handleSubmit() function existance", () => {
         expect(handleSubmit).toBeDefined();
     })
-
-    /*
-    test("testing handleSubmit() function expected output given certain input", () => {
-        const input = "https://jamesclear.com/journaling-one-sentence"
-        const output = {
-
-        }
-        expect(handleSubmit(null, input)).toEqual(output);
-    })
-    */
 })
