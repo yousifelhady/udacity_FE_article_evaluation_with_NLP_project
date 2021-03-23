9af154b6ae33343fda5bb64c6758cbadca013462
yousifelhady <yousif.abdelhamed@gmail.com>
2021-03-23 21:09:46 +0200

    handling server endpoint "add-url" and sending proper response to frontend side

--- src/server/index.js
+++ src/server/index.js
@@ -1,44 +1,50 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+var path = require('path')
+
 const mockAPIResponse = require('./mockAPI.js')
 
-const PORT = 8081
+const PORT = 5000
 
-// TODO add Configuration to be able to use env variables
+const meaningCloudAPIKey = process.env.MEANING_CLOUD_API_KEY
 
 const BASE_API_URL = 'https://api.meaningcloud.com/sentiment-2.1'
 
-// TODO: Create an instance for the server
-// TODO: Configure cors to avoid cors-origin issue
-// TODO: Configure express to use body-parser as middle-ware.
-// TODO: Configure express static directory.
+const express = require('express')
+const cors = require('cors')
+const fetch = require('node-fetch')
+const app = express()
+app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    //res.sendFile('dist/index.html')
+    res.sendFile('src/client/views/index.html')
 })
+
 // INFO: a route that handling post request for new URL that coming from the frontend
 app.post('/add-url', async (req, res) => {
     try {
-        /* TODO:
-    1. GET the url from the request body
-    2. Build the URL it should be something like `${BASE_API_URL}?key=${MEAN_CLOUD_API_KEY}&url=${req.body.url}&lang=en`
-    3. Fetch Data from API
-    4. Send it to the client
-    5. REMOVE THIS TODO AFTER DOING IT 😎😎
-    server sends only specified data to the client with below codes
-     const sample = {
-       text: '',
-       score_tag : '',
-       agreement : '',
-       subjectivity : '',
-       confidence : '',
-       irony : ''
-     }
-  */
+        var submittedUrl = req.body.url
+        var builtUrl = `${BASE_API_URL}?key=${meaningCloudAPIKey}&url=${submittedUrl}&lang=en`
+        console.log(builtUrl)
+        var apiResponse = await fetch(builtUrl)
+        var apiResponseData = await apiResponse.json()
+        
+        var responseObject = {
+            text: apiResponseData.sentence_list[0].text,
+            score_tag : apiResponseData.score_tag,
+            agreement : apiResponseData.agreement,
+            subjectivity : apiResponseData.subjectivity,
+            confidence : apiResponseData.confidence,
+            irony : apiResponseData.irony
+        }
+        console.log(responseObject)
+        res.send(responseObject)
     } catch (error) {
-        console.log(error.message)
+        console.log("error:" + error.message)
     }
 })
 
